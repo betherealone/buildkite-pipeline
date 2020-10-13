fb0a8deda181faf5928fa0fd3a1df7d1ea33df0d
ggsood <gsood@sapient.com>
2020-10-13 22:05:20 +0100

    adding generate targets script

---
+++ .buildkite/generate-targets.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This dynamically generates a pipeline containing a click to unblock step with
+# custom fields.
+
+set -eu
+
+# Separate account lists
+abc=$(find org/management -maxdepth 3 -mindepth 3 -type d | sort)
+
+xyz=$(find org/spoke -maxdepth 3 -mindepth 3 -type d | sort)
+
+result=("${abc[@]}" "${xyz[@]}")
+
+sorted_unique_ids="$(echo "${result[*]}" | tr ' ' '\n' | sort -u )"
+
+## Collect all oth them to a single list
+
+echo 'steps:'
+echo '  - block: "Notify"'
+echo '    fields:'
+echo '      - select: "Which Path should be deployed?"'
+echo '        key: "notify-team"'
+echo '        options:'
+
+for option in $sorted_unique_ids; do
+echo "          - "$option""
+done
+
+echo '  - command: "notify.sh"'
+echo '    label: ":pager:"'
+echo '    agents:'
+echo '      - "queue=testing"'
